4b0569a31f92ccc0250cc5c7a523464d46955999
jordanpeebles <Jordan_Peebles@insideranken.org>
2025-09-29 05:37:50 -0500

    endtend ch 10

--- html-css/data-files/chapter10/extend/scripts/script.js
+++ html-css/data-files/chapter10/extend/scripts/script.js
@@ -1,5 +1,17 @@
 /*
-    Student Name:
+    Student Name: Jordan Peebles
     File Name: script.js
-    Date: 
+    Date: September 29, 2025
 */
+
+$(document).ready(function() {
+    // Hide paragraphs when the "Hide" button is clicked
+    $("button:contains('Hide')").click(function() {
+        $("#main p").hide();
+    });
+
+    // Show paragraphs when the "Show" button is clicked
+    $("button:contains('Show')").click(function() {
+        $("#main p").show();
+    });
+});

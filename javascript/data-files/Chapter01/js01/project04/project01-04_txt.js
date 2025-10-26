cc1c456f20612daa186b8889d8f1b95097908922
jordanpeebles <Jordan_Peebles@insideranken.org>
2025-10-26 18:25:32 -0500

    js ch 1 hands on project

--- javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,19 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Jordan Peebles
+    Date:   10/23/2025
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
+var homeStreet = "1 Main St.";
+var homeCity   = "Sicilia";
+var homeState  = "MA";
+var homeCode   = "02103";
+var workStreet = "15 Oak Ln.";
+var workCity   = "Central City";
+var workState  = "MA";
+var workCode   = "02104";
 
